42e6ade61e38bf316f6551c18ed8abc7dfde6a38
Martin Herrera Ramirez <martin.herrera.ramirez.2016@gmail.com>
2025-08-07 21:43:52 -0300

    proyecto finalizado

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listadoNombreAmigos = [];
+
+function agregarAmigo(){
+    document.getElementById("amigo").value === ""
+    ? alert("Debe ingresar un nombre") 
+    : listadoNombreAmigos.push(nuevoNombre);
+
+    listarAmigos();
+    return;
+}
+
+function listarAmigos(){
+    document.getElementById("listaAmigos").innerHTML = 
+    listadoNombreAmigos.map( amigo => 
+        `<li>${amigo}</li>`).join("\n"
+    ); 
+
+    return;
+}
+
+function sortearAmigo(){       
+    document.getElementById("resultado").innerHTML = `<li>${listadoNombreAmigos[Math.floor(Math.random() * listadoNombreAmigos.length)]}</li>`;
+    
+    return;
+}
